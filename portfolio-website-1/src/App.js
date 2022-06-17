1dee1461d63b74b1ae0a7e9e7ec40b820107c04e
Mohsen9april <Mohsen9april@gmail.com>
2022-06-17 12:35:39 +0430

    Navbar completed

---
+++ portfolio-website-1/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import Header from './components/Header'
+import './App.css';
+
+
+
+const App = () => {
+  return (
+    <div>
+      <Header/>
+    </div>
+  )
+}
+
+export default App
+
+
+
+// function App() {
+//   return (
+//     <div className="App">
+//     </div>
+//   );
+// }
+
+// export default App;
