90ae7554f2ae6a64af30da0844bbaf641f50502a
Mohsen9april <Mohsen9april@gmail.com>
2022-06-19 13:50:07 +0430

    create portfolio section

--- portfolio-website-1/src/components/portfolio/Portfolio.jsx
+++ portfolio-website-1/src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,47 @@
 import React from 'react'
+import data from './data'
 
 const Portfolio = () => {
   return (
     <>
-      <h3>Hello</h3>
+      <section className='portfolio mtop'>
+        <div className='container'>
+          <div className='head flexSB'>
+            <div className='left'>
+              <span className='baget'> Portfolio</span>
+              <h1 className='title'>Creative Portfolio</h1>
+            </div>
+
+            <div className='links'>
+              <ul className='flexSB'>
+                <li className='active'>All</li>
+                <li>Design</li>
+                <li>Brandibg</li>
+                <li>Photography</li>
+              </ul>
+            </div>
+          </div>
+
+          <div className='content grid mtop'>
+            {data.map((elem) => {
+              // const { cover, title, category } = elem
+
+              return (
+                <div className='box'>
+                  <div className='img'>
+                    <img src={elem.cover} alt='' />
+                  </div>
+                  <div className='overlay'>
+                    <h1>{elem.title}</h1>
+                    <p>{elem.category}</p>
+                  </div>
+                </div>
+              )
+            })}
+          </div>
+
+        </div>
+      </section>
     </>
   )
 }
