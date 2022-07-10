b52f3b831d8b743f2ae4f26c14390acdc201d3a1
Mohsen9april <Mohsen9april@gmail.com>
2022-07-10 16:48:17 +0430

    finish left side

--- fit-club-reactjs/src/components/Hero/Hero.jsx
+++ fit-club-reactjs/src/components/Hero/Hero.jsx
@@ -14,16 +14,44 @@ const Hero = () => {
                 <span>The best fitness club i the town</span>
             </div>
 
-            <div className="hero-text"></div>
+            <div className="hero-text">
+                <div>
+                  <span className='stroke-text'>Shape </span>
+                  <span> Your</span>
+                </div>
+                <div>
+                  <span>Ideal Body</span>
+                </div>
+                <div className='span-hero-text'>
+                  <span>In here we will help you to shape and build your ideal body and live up your life to fullest</span>
+                </div>
+            </div>
+
+            <div className='figures'>
+              <div>
+                <span>+140</span>
+                <span>expert coachs</span>
+              </div>
+              <div>
+                <span>+978</span>
+                <span>members joined</span>
+              </div>
+              <div>
+                <span>+50</span>
+                <span>fitness programs</span>
+              </div>
+            </div>
+
+            <div className='hero-buttons'>
+              <button className='btn'>Get Started</button>
+              <button className='btn'>Learn More</button>
+            </div>
         
 
         </div>
-
-
-
         <div className="right-h">right-side</div>
     </div>
-  )
-}
+  );
+};
 
 export default Hero
